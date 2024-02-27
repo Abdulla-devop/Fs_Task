5c9565083a4f64489b77d852c15c8da99a57e0da
Abdulla-devop <abdullaabdullajafar@gmail.com>
2024-02-27 11:10:04 +0530

    Comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+// indexrouter is the content
 import IndexRouter from './src/Routes/index.js'
+// using port
 const PORT = process.env.PORT || 8090
 const app = express()
-
+//api
 app.use('/',IndexRouter)
 
 app.listen(PORT,()=>{
